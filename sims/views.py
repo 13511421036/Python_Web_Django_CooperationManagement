d1efac233520072959bfc2184b56d571f405a3aa
D1zzy <chnzhuziyan@gmail.com>
2023-12-12 17:33:22 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	sims/urls.py
    #	sims/views.py
    #	templates/task/view_task.html

--- sims/views.py
+++ sims/views.py
@@ -14,6 +14,7 @@ def hello(request):
 #########################################################################################################################
 
 # 管理员相关
+# （增加特权管理员，可以删除普通管理员，修改普通管理员密码）
 def administrator_login(request):
     if request.method == 'GET':
         return render(request, 'main/administrator_login.html')
@@ -43,6 +44,7 @@ def administrator_login(request):
 
 
 # 创建新管理员
+# （未实现判断管理员id重复，普通管理员不能创建新管理员）
 def create_administrator(request):
     if request.method == 'GET':
         return render(request, 'main/create_administrator.html')
@@ -70,6 +72,7 @@ def create_administrator(request):
 #########################################################################################################################
 # 员工相关
 # 员工登录
+# （增加员工是否为任务负责人，组负责人判断，若是则可以提交任务或新增组员）
 def worker_login(request):
     if request.method == 'GET':
         return render(request, 'main/worker_login.html')
@@ -100,7 +103,7 @@ def worker_login(request):
 
 
 # 新增员工函数
-# 未实现重复工号返回
+# （未实现重复工号返回）
 def add_worker(request):
     if request.method == 'GET':
         return render(request, 'worker/add_worker.html')
@@ -134,6 +137,7 @@ def add_worker(request):
 
 
 # 员工信息修改函数（管理员视角）
+# （工号不允许修改）
 def edit_worker(request):
     if request.method == 'GET':
         worker_no = request.GET.get("worker_no")
@@ -169,26 +173,33 @@ def edit_worker(request):
                           {'workers': worker, 'groups': group, 'tasks': task})
 
 
-# 员工信息删除函数(未实现判重)
+# 员工信息删除函数
 def delete_worker(request):
     worker_no = request.GET.get("worker_no")
     conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="cooperation", charset='utf8')
     with conn.cursor(cursorclass=MySQLdb.cursors.DictCursor) as cursor:
         # 检查该员工是否为某任务负责人
         cursor.execute(
-            "SELECT task_no FROM sims_task WHERE task_leader = %s ",
+            "SELECT task_no,task_leader FROM sims_task WHERE task_leader = %s ",
             [worker_no])
         account = cursor.fetchone()
         if account:
-            return render(request, 'task/edit_task.html', {'task_no': account[0][1]})
+            cursor.execute("SELECT * FROM sims_task where task_leader =%s", [account['task_leader']])
+            task = cursor.fetchone()
+            return render(request, 'task/edit_task.html',
+                          {'task': task, 'show_popup': True,
+                           'popup_message': '该员工为某任务负责人，请先修改任务负责人'})
 
         # 检查该员工是否为某组负责人
         cursor.execute(
-            "SELECT group_no FROM sims_group WHERE group_leader = %s ",
+            "SELECT group_no,group_leader FROM sims_group WHERE group_leader = %s ",
             [worker_no])
         account = cursor.fetchone()
         if account:
-            return render(request, 'group/edit_group.html', {'group_no': account[0][1]})
+            cursor.execute("SELECT * FROM sims_group where group_leader =%s", [account['group_leader']])
+            group = cursor.fetchone()
+            return render(request, 'group/edit_group.html',
+                          {'group': group, 'show_popup': True, 'popup_message': '该员工为某组负责人，请先修改组负责人'})
 
         # 删除关系表中的信息
         cursor.execute("DELETE FROM sims_workertogroups WHERE worker_id =%s", [worker_no])
@@ -211,7 +222,10 @@ def delete_worker(request):
 
 #########################################################################################################################
 # 任务相关
+
+
 # 新增任务函数
+# （未实现任务号判重，或者任务号自添加？）
 def add_task(request):
     if request.method == 'GET':
         return render(request, 'task/add_task.html')
@@ -270,7 +284,7 @@ def edit_task(request):
         with conn.cursor(cursorclass=MySQLdb.cursors.DictCursor) as cursor:
             cursor.execute("SELECT * FROM sims_task where task_no =%s", [task_no])
             task = cursor.fetchone()
-        return render(request, 'task/edit_task.html', {'task': task})
+        return render(request, 'task/edit_task.html', {'task': task}, 'show_popup', False)
     else:
         task_no = request.POST.get('task_no', '')
         task_information = request.POST.get('task_information', '')
@@ -438,7 +452,7 @@ def edit_group(request):
         with conn.cursor(cursorclass=MySQLdb.cursors.DictCursor) as cursor:
             cursor.execute("SELECT * FROM sims_group where group_no =%s", [group_no])
             group = cursor.fetchone()
-        return render(request, 'group/edit_group.html', {'group': group})
+        return render(request, 'group/edit_group.html', {'group': group}, 'show_popup', False)
     else:
         group_no = request.POST.get('group_no', '')
         group_information = request.POST.get('group_information', '')
